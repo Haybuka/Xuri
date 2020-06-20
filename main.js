dbb866f98998e5ca9fd7825843c27ee8ea1e3a26
Haybuka <paschal.nduleo@gmail.com>
2020-06-20 23:16:49 +0100

    added drop functions

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const burger = document.querySelector('nav#sectionNav .harmburger')
 const dropDown = document.querySelector('nav#sectionNav ul')
-burger.addEventListener('click', () => {
-    burger.classList.add('active')
+const dropSpan = document.querySelectorAll('nav#sectionNav .harmburger span')
+const midSpan = document.querySelector('nav#sectionNav .harmburger span:nth-child(2)')
+const firstSpan = document.querySelector('nav#sectionNav .harmburger span:nth-child(1)')
+const thirdSpan = document.querySelector('nav#sectionNav .harmburger span:nth-child(3)')
+burger.addEventListener('click', function () {
+    this.classList.add('active')
     dropDown.classList.toggle('active')
-    console.log(burger)
+    midSpan.classList.toggle('drop')
+    firstSpan.classList.toggle('cross')
+    thirdSpan.classList.toggle('revCross')
 })
